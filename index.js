27411492ee354506ca9b9f6fca9f1ed36ac845c1
Viktoriia <krjuchenkova@gmail.com>
2020-10-27 20:47:50 +0200

    finished

---
+++ index.js
@@ -0,0 +1,32 @@
+const refs = {
+  startBtn: document.querySelector('button[data-action="start"'),
+  stopBtn: document.querySelector('button[data-action="stop"'),
+},
+colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+],
+randomIntegerFromInterval = (max) => {
+  return Math.floor(Math.random() * (max + 1));
+},
+setRandomColor = () => {
+  const _color = colors[randomIntegerFromInterval(colors.length - 1)]
+  console.log(_color)
+  document.body.style.backgroundColor = _color;
+}
+
+let interval = undefined
+
+refs.startBtn.addEventListener('click', e => { interval =
+setInterval(() => setRandomColor(), 1000);
+refs.startBtn.setAttribute("disabled", true);
+})
+
+refs.stopBtn.addEventListener('click', e => {
+  clearInterval(interval);
+  refs.startBtn.removeAttribute("disabled");
+})
